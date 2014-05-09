dd206b047011ab0a3a46888bbe9394104360f8e8
davew1977 <davew1977@f0745cfb-85a7-4405-84c8-355c0f261dc2>
2014-05-09 20:14:28 +0000

    major work on namespaces

---
+++ xapp-domain/src/main/java/net/sf/xapp/objectmodelling/core/NamespacePath.java
@@ -0,0 +1,15 @@
+package net.sf.xapp.objectmodelling.core;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: davidw
+ * Date: 5/9/14
+ * Time: 7:43 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class NamespacePath extends LinkedList<ObjectMeta> {
+
+}
