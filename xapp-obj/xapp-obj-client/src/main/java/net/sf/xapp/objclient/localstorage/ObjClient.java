71ed82ecc4d0b012d4dc2caa5ea2c4eccea02387
davew1977 <davew1977@f0745cfb-85a7-4405-84c8-355c0f261dc2>
2014-10-01 07:21:25 +0000

    refacted obj client, made generic

--- xapp-obj/xapp-obj-client/src/main/java/net/sf/xapp/objclient/localstorage/ObjClient.java
+++ xapp-obj/xapp-obj-client/src/main/java/net/sf/xapp/objclient/localstorage/ObjClient.java
@@ -7,11 +7,28 @@ import java.io.OutputStreamWriter;
 import java.nio.charset.Charset;
 import java.util.List;
 
+import net.sf.xapp.application.api.SimpleApplication;
+import net.sf.xapp.application.core.ApplicationContainerImpl;
+import net.sf.xapp.application.core.DefaultGUIContext;
 import net.sf.xapp.application.strategies.SaveStrategy;
 import net.sf.xapp.marshalling.Unmarshaller;
+import net.sf.xapp.net.api.chatapp.ChatApp;
+import net.sf.xapp.net.api.chatclient.ChatClient;
+import net.sf.xapp.net.api.chatuser.ChatUser;
+import net.sf.xapp.net.client.framework.Callback;
+import net.sf.xapp.net.client.io.HostInfo;
+import net.sf.xapp.net.client.io.ServerProxyImpl;
+import net.sf.xapp.net.common.types.ErrorCode;
+import net.sf.xapp.net.common.types.UserId;
+import net.sf.xapp.objclient.IncomingChangesAdaptor;
+import net.sf.xapp.objclient.NodeUpdateApiRemote;
+import net.sf.xapp.objclient.ObjClientContext;
+import net.sf.xapp.objclient.ui.ChatPane;
 import net.sf.xapp.objcommon.SimpleObjUpdater;
 import net.sf.xapp.objectmodelling.api.ClassDatabase;
 import net.sf.xapp.objectmodelling.core.ObjectMeta;
+import net.sf.xapp.objserver.apis.objlistener.ObjListener;
+import net.sf.xapp.objserver.apis.objmanager.ObjManagerReply;
 import net.sf.xapp.objserver.types.Delta;
 import net.sf.xapp.objserver.types.XmlObj;
 import net.sf.xapp.utils.FileUtils;
@@ -21,9 +38,10 @@ import net.sf.xapp.utils.ant.AntFacade;
  * © 2013 Newera Education Ltd
  * Created by dwebber
  */
-public class LocalStore implements SaveStrategy{
+public class ObjClient implements SaveStrategy, ObjManagerReply {
     public static final String LOCAL_DIR = System.getProperty("user.home", ".") + "/xapp-cache";
-    private final File dir;
+    private final ObjClientContext clientContext;
+    private final String objId;
     private final File revFile;
     private final File objFile;
     private final File deltaFile;
@@ -33,13 +51,25 @@ public class LocalStore implements SaveStrategy{
     private ObjectMeta objMeta;
     private ClassDatabase cdb;
 
-    public LocalStore(String user, String appId, String objId) {
-        dir = new File(new File(new File(LOCAL_DIR, user), appId), objId);
+    public ObjClient(String userId, HostInfo hostInfo, String appId, String objId) {
+        this.clientContext = new ObjClientContext(userId, new ServerProxyImpl(hostInfo));
+        this.objId = objId;
+        File dir = new File(new File(new File(LOCAL_DIR, userId), appId), objId);
         dir.mkdirs();
         revFile = new File(dir, "rev.txt");
         objFile = new File(dir, "obj.xml");
         deltaFile = new File(dir, "deltas.txt");
         initialDeltas = FileUtils.readFile(deltaFile, Charset.forName("UTF-8")).split("\n");
+    }
+
+    public void init() {
+        clientContext.connect();
+        clientContext.login();
+
+        clientContext.wire(ObjManagerReply.class, objId, this);
+
+        clientContext.channel(objId).join(clientContext.getUserId());
+        clientContext.objManager(objId).getObject(clientContext.getUserId());
     }
 
     public long getLastKnownRevision() {
@@ -114,5 +144,50 @@ public class LocalStore implements SaveStrategy{
 
     public ClassDatabase getCdb() {
         return cdb;
+    }
+
+
+    @Override
+    public void getObjectResponse(UserId principal, XmlObj obj, ErrorCode errorCode) {
+
+        System.out.println(obj.getData());
+
+        reset(obj);
+        lauchGUI();
+    }
+
+    @Override
+    public void getDeltasResponse(UserId principal, List<Delta> deltas, Class type, Long revTo, ErrorCode errorCode) {
+        reconstruct(type, deltas, revTo);
+        lauchGUI();
+    }
+
+    private void lauchGUI() {
+        final ChatPane chatPane = new ChatPane(clientContext);
+        clientContext.wire(ChatClient.class, objId, chatPane);
+        clientContext.wire(ChatUser.class, objId, chatPane);
+        final ChatApp chatApp = clientContext.chatApp(objId);
+        chatPane.addListener(new Callback() {
+            @Override
+            public <T> T call(Object... args) {
+                chatApp.newChatMessage(clientContext.getUserId(), (String) args[0]);
+                return null;
+            }
+        });
+        chatPane.setSize(200,300);
+
+        ApplicationContainerImpl appContainer = new ApplicationContainerImpl(new DefaultGUIContext(new File("file.xml"), cdb, objMeta));
+        appContainer.setSaveStrategy(this);
+        appContainer.add(chatPane, "bottomLeft");
+        appContainer.setUserGUI(new SimpleApplication());
+        appContainer.getMainFrame().setVisible(true);
+
+        IncomingChangesAdaptor incomingChangesAdaptor = new IncomingChangesAdaptor(appContainer, clientContext);
+        appContainer.setNodeUpdateApi(new NodeUpdateApiRemote(cdb, clientContext, objId, incomingChangesAdaptor));
+        clientContext.wire(ObjListener.class, objId, incomingChangesAdaptor);
+    }
+
+    public static void main(String[] args) {
+        new ObjClient(args[0], HostInfo.parse(args[1]), args[2], args[3]);
     }
 }
